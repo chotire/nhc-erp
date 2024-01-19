c1c84b067756ebeb118cf5a003a8825dba3fb32d
DESKTOP-1D0JO07\sdcit-yilee <infiad@sdcit.co.kr>
2024-01-19 10:20:03 +0900

    이용일 > 소스정리

---
+++ src/main/java/nhc/erp/prunus/framework/authentication/LoginSuccessHandler.java
@@ -0,0 +1,26 @@
+package nhc.erp.prunus.framework.authentication;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 이용일
+ */
+@Component
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+   
+}
