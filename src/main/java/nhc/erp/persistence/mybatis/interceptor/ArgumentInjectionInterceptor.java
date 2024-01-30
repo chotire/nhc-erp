097614c09cc7f2531671b4ceadd09242973a25cf
DESKTOP-1D0JO07\sdcit-yilee <infiad@sdcit.co.kr>
2024-01-30 09:24:50 +0900

    이용일 > sql 실행 전 Argument 주입을 위한 인터셉터 커밋

---
+++ src/main/java/nhc/erp/persistence/mybatis/interceptor/ArgumentInjectionInterceptor.java
@@ -0,0 +1,26 @@
+package nhc.erp.persistence.mybatis.interceptor;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+@Intercepts({
+	@Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+	@Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
+})
+public class ArgumentInjectionInterceptor implements Interceptor {
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		Object[] args = invocation.getArgs();
+		Object param = args[1];
+		
+		//Todo... sql 실행 전 공통된 Argument들을 주입하기 주입하기 위한 인터셉터 처리
+		
+		return invocation.proceed();
+	}
+}
